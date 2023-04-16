cffdb665843734c859957d1a2b11937d9901a0a1
FabPaul <paulfopa0@gmail.com>
2023-04-16 05:17:40 -0700

    header files with all library functions

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <signal.h>
+#include <sys/stat.c>
+#include < fcntl.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <stddef.h>
+
+
+#endif
